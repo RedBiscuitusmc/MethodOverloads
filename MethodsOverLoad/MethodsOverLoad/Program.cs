3ee7811950851443971f2e832d8ce92036425121
Jake Merlin <JacobMerlin92@outlook.com>
2022-07-11 14:00:31 -0400

    First Push

---
+++ MethodsOverLoad/MethodsOverLoad/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace OverLoad
+/*.=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -.
+/------------------------------ \
+|     By                                     |
+|     Jacob Merlin                   |
+\------------------------------ /
+             !           ! 
+            !           !
+            !          !
+           ! _       !
+     /   !__)     !
+__/___/_____)
+    (_______)
+___(______)
+  \_(____)
+     !       !
+     !      !
+     \__/
+'-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='*/
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var snum_1 = 4;
+            var snum_2 = 2;
+            logic_Sonic.SonicLogic.AddMethod(snum_1, snum_2);
+            var totalAdd = logic_Sonic.SonicLogic.AddMethod(snum_1, snum_2);
+            Console.WriteLine(totalAdd);
+
+            var snum_3 = 4.2m;
+            var snum_4 = 2.6m;
+            logic_Sonic.SonicLogic.AddMethod(snum_3, snum_4);
+            var totalAddFloat = logic_Sonic.SonicLogic.AddMethod(snum_3, snum_4);
+            Console.WriteLine(totalAddFloat);
+
+            var snum_5 = 42;
+            var snum_6 = 52;
+            var IsPayCheck = true;
+            logic_Sonic.SonicLogic.AddMethod(snum_5, snum_6, IsPayCheck);
+            var total = logic_Sonic.SonicLogic.AddMethod(snum_5, snum_6, IsPayCheck);
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ MethodsOverLoad/MethodsOverLoad/SonicLogic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+/*.=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -.
+   /------------------------------ \
+  |     By                                     |
+  |     Jacob Merlin                   |
+   \------------------------------ /
+                 !           ! 
+                !           !
+                !          !
+               ! _       !
+         /   !__)     !
+ __/___/_____)
+        (_______)
+ ___(______)
+      \_(____)
+         !       !
+         !      !
+         \__/
+'-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='*/
+
+namespace logic_Sonic
+{
+	public class SonicLogic
+	{
+		public static int AddMethod(int snum_1, int snum_2)
+        {
+			return snum_1 + snum_2;
+        }
+
+		public static decimal AddMethod(decimal snum_3, decimal snum_4)
+        {
+			return (snum_3 + snum_4);
+        }
+		
+		public static string AddMethod(int snum_5, int snum_6, bool IsPayCheck)
+		{
+			int num_Total = snum_5 + snum_6;
+
+			if (IsPayCheck == true && num_Total > 1 || num_Total == 0)
+			{
+				return $"{num_Total} dollars";
+			}
+			else if (IsPayCheck == true && num_Total == 1)
+			{
+				return $" {num_Total} dollar";
+			}
+			else
+            {
+				return num_Total.ToString();
+            }
+		}
+	}
+}
